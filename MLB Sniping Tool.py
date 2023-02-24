e138f4910c332ae5eb965e40ef981b997df9bfc6
Chimaezss <cheesewithease@outlook.com>
2023-02-24 10:32:34 -0500

    added card list

--- MLB Sniping Tool.py
+++ MLB Sniping Tool.py
@@ -1,9 +1,16 @@
+card_list = []
+
 while True:
     # Required information for program
-    test_price = input('What can you find the card for? ')
-    buy_price = (int(test_price) * .90)
-    print(f'Diamond Card: {buy_price - 500}.')
-    print(f'Gold Card: {buy_price - 250}.')
-    print(f'Silver Card: {buy_price - 100}.')
-    # Here would be a prompt to quit, not displayed to reduce clutter
+    test_price = input('What can you find the card for? ') # Here would be a prompt to quit, not displayed to reduce clutter
     if test_price == '-1': break
+    elif test_price == '-2': # Prompt for player info would be here, not present for less clutter
+        player_info = input('Enter player information: ')
+        card_list.append(player_info)
+    elif test_price == '-3':
+        print(card_list)
+    else:
+        buy_price = (int(test_price) * .90)
+        print(f'Silver Card: {buy_price - 100}.')
+        print(f'Gold Card: {buy_price - 250}.')
+        print(f'Diamond Card: {buy_price - 500}.')
---
+++ Project Plans.md
@@ -0,0 +1,7 @@
+Plans:
+    1) Create a flipping tool to find a profitable price after tax
+    2) Setup system to track cards - preliminary idea to just have a list in CLI
+    3) Functionality for adding and removing from list
+    4) Create and link database with all cards to be able to track easier
+    5) Move project from CLI to different UI, either GUI or Website
+    6) Possibly explore other options to turn tool into more of a companion than just a snipe tool
